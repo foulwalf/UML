b306ee96e334bb9350bcaf5f673fcbac92c0ee75
Raymond <raglehy@outlook.fr>
2021-02-23 16:41:33 +0000

    partage 23/02/2021

---
+++ views/accueil.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8"/>
+    <title>ACCUEIL</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/accueil.css">
+  </head>
+  <body>
+  
+  <div class="entete"> 
+    <table>
+      <tr>
+        <td><img src="assets/img/logo_AGITEL.png"></td>
+        <td>Se connecter</td>
+      </tr>
+      
+    </table>
+  </div> 
+
+  <div class="bouton"> 
+    <!-- Bouton d'inscription -->
+    <button onclick="window.location.href = 'inscription.php'" >S'inscrire</button>
+  
+  </div> 
+
+  </body>
+</html>
